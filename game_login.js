3cabcae8e03dfdc19e9baa8534073139dd425ec5
SohamDasIsCool <72563026+SohamDasIsCool@users.noreply.github.com>
2020-11-24 21:25:04 +0530

    Add files via upload

--- game_login.js
+++ game_login.js
@@ -5,3 +5,14 @@ function addUser() {
     localStorage.setItem("player2_name",player2_name);
     window.location.replace("game_page.html");
 }
+function send() {
+get_number1=document.getElementById("number1").value;
+get_number2=document.getElementById("number2").value;
+equation=get_number1+"x"+get_number2;
+    question_word="<h4 id='word_display'>Q."+equation+"</h4>";
+    input_box="<br>Answer: <input type='number' id='input_check_box'>";
+    check_button="<br><br><button class='btn btn-info' onclick='check()'>Check</button>";
+    row=question_word+input_box+check_button;
+    document.getElementById("output").innerHTML=row;
+    document.getElementById("word").value="";
+}
