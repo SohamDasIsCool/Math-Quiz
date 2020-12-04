12010ce799721e16eef2c8f71159a18d5513d790
SohamDasIsCool <72563026+SohamDasIsCool@users.noreply.github.com>
2020-12-04 19:56:39 +0530

    Add files via upload

--- game_login.js
+++ game_login.js
@@ -4,6 +4,8 @@ function addUser() {
     localStorage.setItem("player1_name",player1_name);
     localStorage.setItem("player2_name",player2_name);
     window.location.replace("game_page.html");
+    answer_turn == "player1";
+    question_turn == "player2";
 }
 function send() {
 get_number1=document.getElementById("number1").value;
@@ -14,5 +16,54 @@ equation=get_number1+"x"+get_number2;
     check_button="<br><br><button class='btn btn-info' onclick='check()'>Check</button>";
     row=question_word+input_box+check_button;
     document.getElementById("output").innerHTML=row;
+    document.getElementById("number1").value="";
+    document.getElementById("number2").value="";
+}
+function check() {
+    get_word=document.getElementById("word").value;
+    word=get_word.toLowerCase();
+    word=get_word*get_word;
+    question_word="<h4 id='word_display'>Q."+remove_charAt3+"</h4>";
+    input_box="<br>Answer: <input type='text' id='input_check_box'>";
+    check_button="<br><br><button class='btn btn-info' onclick='check()'>Check</button>";
+    row=question_word+input_box+check_button;
+    document.getElementById("output").innerHTML=row;
     document.getElementById("word").value="";
 }
+function check() {
+    get_answer=document.getElementById("input_check_box").value;
+    answer=get_answer.toLowerCase();
+    console.log("answer in lower case"+answer);
+    document.getElementById("player1_name").innerHTML=player1_name+":";
+    document.getElementById("player2_name").innerHTML=player2_name+":";
+    if(answer==word && answer_turn == "player1") {
+        player1_score += 1;
+        document.getElementById("player1_score").innerHTML=player1_score;
+        answer_turn="player2";
+        question_turn="player1";
+        document.getElementById("player_question").innerHTML=question_turn;
+        document.getElementById("player_answer").innerHTML=answer_turn;
+    }  
+        else {
+            player2_score += 1;
+            document.getElementById("player2_score").innerHTML=player2_score;
+            answer_turn="player1";
+            question_turn="player2";
+            document.getElementById("player_question").innerHTML=question_turn;
+            document.getElementById("player_answer").innerHTML=answer_turn;
+        }
+    }
+    if(answer != word && answer_turn == "player1") {
+        if(answer_turn == "player1") {
+            answer_turn="player2";
+            question_turn="player1";
+            document.getElementById("player_question").innerHTML=question_turn;
+            document.getElementById("player_answer").innerHTML=answer_turn;
+        }
+        else {
+            answer_turn="player1";
+            question_turn="player2";
+            document.getElementById("player_question").innerHTML=question_turn;
+            document.getElementById("player_answer").innerHTML=answer_turn;
+        }
+    }
